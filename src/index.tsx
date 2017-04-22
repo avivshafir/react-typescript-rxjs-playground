0b4bb900bec4f329931204c0915e99e1615f6b0c
avivshafir <aviv@innovid.com>
2017-04-22 19:49:32 +0300

    add stocks

--- src/index.tsx
+++ src/index.tsx
@@ -1,52 +1,34 @@
 import * as React from "react";
 import { render } from "react-dom";
-import * as Rx from "rxjs/Rx";
-import * as R from "ramda";
 
-class App extends React.Component<any, any> {
-  constructor(props) {
-    super();
-    this.state = {
-      results: []
-    };
-  }
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+import Stocks from "./Stocks";
+import Wikipedia from "./Wikipedia";
 
-  componentDidMount() {
-    const searchBox: any = document.querySelector('#search');
-    const URL = 'https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=';
-    Rx.Observable
-      .fromEvent(searchBox, 'keyup')
-      .pluck('target', 'value')
-      .debounceTime(500)
-      .filter(val => val && val.toString().length > 0)
-      .do(term => console.log(`Searching with term ${term}`))
-      .map(query => URL + query)
-      .mergeMap(query => Rx.Observable.ajax(query)
-        .pluck('response', 'query', 'search')
-        .defaultIfEmpty([]))
-      .do(console.log)
-      .mergeMap(R.map(R.prop('title')))
-      .do(console.log)
-      .subscribe(results => {
-        console.log("setting state: ", [...this.state.results, results])
-        this.setState({ results: [...this.state.results, results] })
-      });
-  }
+const Home = () => (
+  <h1>React Redux RxJS Examples</h1>
+)
 
-  render() {
-    return (
-      <div style={{ textAlign: "center" }}>
-        <input id="search" />
-        <ul style={{ listStyle: "none" }}>
-          {this.state.results.map((title, id) => (
-            <li key={id}>
-              {title}
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/wikipedia">Wikipedia</Link></li>
+        <li><Link to="/stocks">Stocks</Link></li>
+      </ul>
+
+      <hr />
+
+      <Route exact path="/" component={Home} />
+      <Route path="/wikipedia" component={Wikipedia} />
+      <Route path="/stocks" component={Stocks} />
+    </div>
+  </Router>
+)
 
 render(<App />, document.getElementById("root"));
